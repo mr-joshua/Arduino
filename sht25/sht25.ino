773d5c54aca2b01728e5d1b7cfd530acc7b68be2
mr-joshua <josh@bov33.com>
2017-01-23 22:36:33 -0600

    added sht25 sensor v0.1 for beta and accuracy testing against dht11

---
+++ sht25/sht25.ino
@@ -0,0 +1,179 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0: Henrik EKblad
+ * Version 1.1 - 2016-07-20: Converted to MySensors v2.0 and added various improvements - Torben Woltjen (mozzbozz)
+ * 
+ * DESCRIPTION
+ * This sketch provides an example of how to implement a humidity/temperature
+ * sensor using a DHT11/DHT-22.
+ *  
+ * For more information, please visit:
+ * http://www.mysensors.org/build/humidity
+ * 
+ */
+
+// Enable debug prints
+#define MY_DEBUG
+
+// Enable and select radio type attached 
+#define MY_RADIO_NRF24
+ 
+#include <SPI.h>
+#include <MySensors.h>  
+#include<Wire.h>
+
+// SHT25 I2C address is 0x40(64)
+#define Addr 0x40
+
+// Sleep time between sensor updates (in milliseconds)
+// Must be >1000ms for DHT22 and >2000ms for DHT11
+static const uint64_t UPDATE_INTERVAL = 60000;
+
+// Force sending an update of the temperature after n sensor reads, so a controller showing the
+// timestamp of the last update doesn't show something like 3 hours in the unlikely case, that
+// the value didn't change since;
+// i.e. the sensor would force sending an update every UPDATE_INTERVAL*FORCE_UPDATE_N_READS [ms]
+static const uint8_t FORCE_UPDATE_N_READS = 10;
+
+#define CHILD_ID_HUM 0
+#define CHILD_ID_TEMP 1
+
+float lastTemp;
+float lastHum;
+uint8_t nNoUpdatesTemp;
+uint8_t nNoUpdatesHum;
+////bool metric = true;
+bool metric = false;
+
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+
+void presentation()  
+{ 
+  // Send the sketch version information to the gateway
+  sendSketchInfo("SHT25 TempAndHumidity", "0.1");
+  
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_HUM, S_HUM);
+  present(CHILD_ID_TEMP, S_TEMP);
+}
+
+void setup()
+{
+  // Initialise I2C communication as MASTER
+  Wire.begin();
+  // Initialise serial communication, set baud rate = 9600
+  Serial.begin(9600);
+  delay(300);
+}
+
+
+void loop()      
+{  
+    unsigned int data[2];
+    float temperature;
+    float humidity;
+
+  // Get temperature from SHT25
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send temperature measurement command, NO HOLD master
+  Wire.write(0xF3);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Request 2 bytes of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // temp msb, temp lsb
+  if(Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+
+    // Convert the data
+    float cTemp = (((data[0] * 256.0 + data[1]) * 175.72) / 65536.0) - 46.85;
+    temperature = (cTemp * 1.8) + 32;
+  }
+
+  if (isnan(temperature)) {
+    Serial.println("Failed reading temperature from SHT25!");
+  } else if (temperature != lastTemp || nNoUpdatesTemp == FORCE_UPDATE_N_READS) {
+    // Only send temperature if it changed since the last measurement or if we didn't send an update for n times
+    lastTemp = temperature;
+    nNoUpdatesTemp = 0;
+    send(msgTemp.set(temperature, 1));
+
+    #ifdef MY_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  } else {
+    // Increase no update counter if the temperature stayed the same
+    nNoUpdatesTemp++;
+  }
+
+  // Get humidity from SHT25
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Send humidity measurement command, NO HOLD master
+  Wire.write(0xF5);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Request 2 bytes of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // humidity msb, humidity lsb
+  if(Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+
+    // Convert the data
+    humidity = (((data[0] * 256.0 + data[1]) * 125.0) / 65536.0) - 6;
+  }  
+  if (isnan(humidity)) {
+    Serial.println("Failed reading humidity from DHT");
+  } else if (humidity != lastHum || nNoUpdatesHum == FORCE_UPDATE_N_READS) {
+    // Only send humidity if it changed since the last measurement or if we didn't send an update for n times
+    lastHum = humidity;
+    // Reset no updates counter
+    nNoUpdatesHum = 0;
+    send(msgHum.set(humidity, 1));
+    
+    #ifdef MY_DEBUG
+    Serial.print("H: ");
+    Serial.println(humidity);
+    #endif
+  } else {
+    // Increase no update counter if the humidity stayed the same
+    nNoUpdatesHum++;
+  }
+
+  // Sleep for a while to save energy
+  sleep(UPDATE_INTERVAL); 
+}
